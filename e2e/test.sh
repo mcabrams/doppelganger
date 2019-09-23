be3262d58466efee14a45cbfe9cad021b6e62375
Matthew Abrams <mcabrams1@gmail.com>
2019-09-22 17:28:15 -0700

    Fix e2e tests on CI to ensure returns proper exit code
    - Ensure no cleanup on ci runs for faster tests, and proper exit code
    return

--- e2e/test.sh
+++ e2e/test.sh
@@ -11,6 +11,13 @@ fi
 docker-compose -f docker-compose.yml -f docker-compose.test.yml up -d server-test client-test
 docker-compose -f docker-compose.yml -f docker-compose.test.yml run cypress ./wait-for.sh http://client-test:8080 cypress run
 
+if [[ $? != 0 ]] && [[ $1 = "--no-cleanup" ]]; then
+  echo "Tests include a failure..." >&2
+  exit 1
+else
+  echo "All tests passed!"
+fi
+
 if [[ $1 != "--no-cleanup" ]]; then
   docker-compose -f docker-compose.yml -f docker-compose.test.yml down -v
 fi
