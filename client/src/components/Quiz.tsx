9d69c76e5e676e8e4fef537735e4dddfae40d326
Matthew Abrams <mcabrams1@gmail.com>
2019-09-22 20:55:21 -0700

    Abstract redirect if logged out functionality into a hook

--- client/src/components/Quiz.tsx
+++ client/src/components/Quiz.tsx
@@ -1,26 +1,15 @@
 import { Col, Row, Typography } from 'antd';
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { useDispatch } from 'react-redux';
 
-import { ROUTES } from '@src/constants/routes';
-import { useIsLoggedIn } from '@src/hooks/useIsLoggedIn';
-import { setAuthRedirectLocation } from '@src/redux/authRedirect/actions';
+import { useRedirectIfLoggedOut } from '@src/hooks/useRedirectIfLoggedOut';
 
 type QuizProps = RouteComponentProps;
 
 const { Title } = Typography;
 
 export const Quiz: React.FC<QuizProps> = ({ navigate }) => {
-  const [isLoggedIn, _] = useIsLoggedIn();
-  const dispatch = useDispatch();
-
-  if (!isLoggedIn) {
-    dispatch(setAuthRedirectLocation('/quiz'));
-    if (navigate) {
-      navigate(ROUTES.signup);
-    }
-  }
+  useRedirectIfLoggedOut('/quiz', navigate);
 
   return (
     <Row>
